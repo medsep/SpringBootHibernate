55b0249256c6dc932b052299bdf3e549d3889d7e
medsep <mehdi.sep@gmail.com>
2021-03-14 19:05:42 +1100

    MVC Rest7

---
+++ hb-01-one-to-one-uni/src/com/med/hibernate/demo/entity/InstructorDetail.java
@@ -0,0 +1,71 @@
+package com.med.hibernate.demo.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="instructor_detail")
+public class InstructorDetail {
+		
+	//annoatte the clas as an entity and map to db
+	//define the fields
+	//annotate the fields wit hdb fields
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+
+	@Column(name="youtube_channel")
+	private String youtubeChannel;
+	
+	@Column(name="hobby")
+	private String hobby;
+	
+	
+	//create constructors
+	public InstructorDetail(String youtubeChannel, String hobby) {
+		this.youtubeChannel = youtubeChannel;
+		this.hobby = hobby;
+	}	
+	
+	//generate getter/setter methods
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getYoutubeChannel() {
+		return youtubeChannel;
+	}
+
+
+	public void setYoutubeChannel(String youtubeChannel) {
+		this.youtubeChannel = youtubeChannel;
+	}
+
+
+	public String getHobby() {
+		return hobby;
+	}
+
+
+	public void setHobby(String hobby) {
+		this.hobby = hobby;
+	}
+	//generete toString() method
+
+	@Override
+	public String toString() {
+		return "InstructorDetail [id=" + id + ", youtubeChannel=" + youtubeChannel + ", hobby=" + hobby + "]";
+	}
+	
+}
